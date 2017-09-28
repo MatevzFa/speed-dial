413cf4a85b013d509aa833aac5f942e3618ece9f
matevzfa <mf6422@student.uni-lj.si>
2017-09-28 23:24:37 +0200

    Changes for firefox

--- js/script.js
+++ js/script.js
@@ -1,157 +1,157 @@
 
 function setProfile(profile) {
-  chrome.storage.sync.get(function(data) {
-    data.curProfile = profile;
-    alert("Profile set to <" + profile + ">.");
-    if (!data.profiles) data.profiles = {};
-    if (!data.profiles[profile]) {
-      alert("Initializing new profile.");
-      data.profiles[profile] = {
-        "thumbs": [],
-        "linkbars": {
-          "top-left": {
-            "title" : null,
-            "links" : []
-          },
-          "top-right": {
-            "title" : null,
-            "links" : []
-          },
-          "bottom-right": {
-            "title" : null,
-            "links" : []
-          },
-          "bottom-left": {
-            "title" : null,
-            "links" : []
-          }
+    browser.storage.sync.get(function (data) {
+        data.curProfile = profile;
+        alert("Profile set to <" + profile + ">.");
+        if (!data.profiles) data.profiles = {};
+        if (!data.profiles[profile]) {
+            alert("Initializing new profile.");
+            data.profiles[profile] = {
+                "thumbs": [],
+                "linkbars": {
+                    "top-left": {
+                        "title": null,
+                        "links": []
+                    },
+                    "top-right": {
+                        "title": null,
+                        "links": []
+                    },
+                    "bottom-right": {
+                        "title": null,
+                        "links": []
+                    },
+                    "bottom-left": {
+                        "title": null,
+                        "links": []
+                    }
+                }
+            }
         }
-      }
-    }
-    chrome.storage.sync.set(data, function() {})
-  })
+        browser.storage.sync.set(data, function () { })
+    })
 }
 
 function exportSettings() {
-  chrome.storage.sync.get(function(data) {
-    $("<a />", {
-      download: "speed-dial-" + (new Date()).toISOString().substring(0,19).replace(/\:/g, '-') + ".json",
-      href: "data:application/json," + encodeURIComponent(JSON.stringify(data, null, '  '))
-    }).appendTo("body")
-    .click(function() {
-       $(this).remove()
-    })[0].click()
-  })
+    browser.storage.sync.get(function (data) {
+        $("<a />", {
+            download: "speed-dial-" + (new Date()).toISOString().substring(0, 19).replace(/\:/g, '-') + ".json",
+            href: "data:application/json," + encodeURIComponent(JSON.stringify(data, null, '  '))
+        }).appendTo("body")
+            .click(function () {
+                $(this).remove()
+            })[0].click()
+    })
 }
 
 function importSettings() {
-  if($('body #import-area')) {
-    $div = $('<div>', {id: 'import-area', style: "position: fixed; top: 50px; left: 25%;"})
-    $textarea = $('<textarea>', {rows: "40", cols: "80", placeholder: "JSON", style: "font-size: 10pt"})
-    $btn = $('<button>', {text: "Import"})
-    $('body').append($div)
-    $('body #import-area').append($textarea)
-    $('body #import-area').append($btn);
-    $('body #import-area button').click(function() {
-      if ($('body #import-area textarea').val().length > 0) {
-        chrome.storage.sync.set(JSON.parse($('body #import-area textarea').val()), function() {
-          $('#import-area').remove();
-          loadSetup();
+    if ($('body #import-area')) {
+        $div = $('<div>', { id: 'import-area', style: "position: fixed; top: 50px; left: 25%;" })
+        $textarea = $('<textarea>', { rows: "40", cols: "80", placeholder: "JSON", style: "font-size: 10pt" })
+        $btn = $('<button>', { text: "Import" })
+        $('body').append($div)
+        $('body #import-area').append($textarea)
+        $('body #import-area').append($btn);
+        $('body #import-area button').click(function () {
+            if ($('body #import-area textarea').val().length > 0) {
+                browser.storage.sync.set(JSON.parse($('body #import-area textarea').val()), function () {
+                    $('#import-area').remove();
+                    loadSetup();
+                })
+            } else {
+                $('body #import-area').remove();
+            }
         })
-      } else {
-        $('body #import-area').remove();
-      }
-    })
-  }
+    }
 }
 
 // [{"image": ..., "link": ...}, ...]
 function setProfileThumbs(profile, thumbArrayJSONString) {
-  chrome.storage.sync.get(function(data) {
-    if (!data.curProfile) alert('Profile not set');
-    data.profiles[profile].thumbs = JSON.parse(thumbArrayJSONString)
-    chrome.storage.sync.set(data, function() {
-      alert("Set thumbs for <" + profile + ">.");
+    browser.storage.sync.get(function (data) {
+        if (!data.curProfile) alert('Profile not set');
+        data.profiles[profile].thumbs = JSON.parse(thumbArrayJSONString)
+        browser.storage.sync.set(data, function () {
+            alert("Set thumbs for <" + profile + ">.");
+        });
     });
-  });
 }
 
 // {'top-left': ..., 'top-right': ..., ...}
 function setProfileLinkbars(profile, linkbarArrayJSONString) {
-  chrome.storage.sync.get(function(data) {
-    if (!data.curProfile) alert('Profile not set');
-    data.profiles[profile].linkbars = JSON.parse(linkbarArrayJSONString)
-    chrome.storage.sync.set(data, function() {
-      alert("Set thumbs for <" + profile + ">.");
+    browser.storage.sync.get(function (data) {
+        if (!data.curProfile) alert('Profile not set');
+        data.profiles[profile].linkbars = JSON.parse(linkbarArrayJSONString)
+        browser.storage.sync.set(data, function () {
+            alert("Set thumbs for <" + profile + ">.");
+        });
     });
-  });
 }
 
 function loadThumbs() {
-  $('#thumbnail-container').empty();
-  chrome.storage.sync.get(function(data) {
-    if (!data.curProfile || !data.profiles[data.curProfile].thumbs) return;
-    var thumbnails = data.profiles[data.curProfile];
-    for (let i = 0; i < thumbnails.thumbs.length; i++) {
-      $('#thumbnail-container').append(
-        $('<a />', {
-          class: 'thumbnail ' + (thumbnails.thumbs[i].url == 'empty' ? 'hidden' : 'clickable clickable-href'),
-          href: thumbnails.thumbs[i].url
-        }).append(
-          $('<div />', {
-            class: 'thumbnail-background',
-            style: 'background-image: url(' + linkify(thumbnails.thumbs[i].img) + ')'
-          })
-        )
-      )
-    }
-    $('#thumbnail-container').append($('<div />', { style: 'clear: left; display: block;' }))
-  });
+    $('#thumbnail-container').empty();
+    browser.storage.sync.get(function (data) {
+        if (!data.curProfile || !data.profiles[data.curProfile].thumbs) return;
+        var thumbnails = data.profiles[data.curProfile];
+        for (let i = 0; i < thumbnails.thumbs.length; i++) {
+            $('#thumbnail-container').append(
+                $('<a />', {
+                    class: 'thumbnail ' + (thumbnails.thumbs[i].url == 'empty' ? 'hidden' : 'clickable clickable-href'),
+                    href: thumbnails.thumbs[i].url
+                }).append(
+                    $('<div />', {
+                        class: 'thumbnail-background',
+                        style: 'background-image: url(' + linkify(thumbnails.thumbs[i].img) + ')'
+                    })
+                    )
+            )
+        }
+        $('#thumbnail-container').append($('<div />', { style: 'clear: left; display: block;' }))
+    });
 }
 
-function linkify (url) {
-  var patt = /^(http:\/\/|https:\/\/).+?\.(png|jpg|gif|svg)$/;
-  if (patt.test(url)) {
-    return url
-  } else {
-    return 'assets/images/' + url
-  }
+function linkify(url) {
+    var patt = /^(http:\/\/|https:\/\/).+?\.(png|jpg|gif|svg)$/;
+    if (patt.test(url)) {
+        return url
+    } else {
+        return 'assets/images/' + url
+    }
 }
 
 function loadLinkbars() {
-  chrome.storage.sync.get(function(data) {
-    if (!data.curProfile || !data.profiles[data.curProfile].linkbars) return;
-    var linkbars = data.profiles[data.curProfile].linkbars;
-    for (barname in linkbars) {
-      if(linkbars[barname].title) {
-        $("#links-bar-" + barname).append(linkbars[barname].title + ':&nbsp;&nbsp;');
-      }
-      for (var i = 0; i < linkbars[barname].links.length; i++) {
-        $("#links-bar-" +
-          barname).append((i == 0 ? '' : ' | ') +
-          '<a class="links-bar-node clickable-href" href="' + linkbars[barname].links[i].url + '">' + linkbars[barname].links[i].name + '</a>'
-        );
-      }
-    }
-  });
+    browser.storage.sync.get(function (data) {
+        if (!data.curProfile || !data.profiles[data.curProfile].linkbars) return;
+        var linkbars = data.profiles[data.curProfile].linkbars;
+        for (barname in linkbars) {
+            if (linkbars[barname].title) {
+                $("#links-bar-" + barname).append(linkbars[barname].title + ':&nbsp;&nbsp;');
+            }
+            for (var i = 0; i < linkbars[barname].links.length; i++) {
+                $("#links-bar-" +
+                    barname).append((i == 0 ? '' : ' | ') +
+                    '<a class="links-bar-node clickable-href" href="' + linkbars[barname].links[i].url + '">' + linkbars[barname].links[i].name + '</a>'
+                    );
+            }
+        }
+    });
 }
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('#thumbnail-container').bind('DOMNodeInserted DOMNodeRemoved', function () {
-     $('#thumbnail-container').css('margin-top', $(window).height()/2 - $('#thumbnail-container').height()/2);
-     $('.thumbnail').css('height', $('.thumbnail').width()/16*9);
-  });
-  loadThumbs();
-  loadLinkbars();
+    $('#thumbnail-container').bind('DOMNodeInserted DOMNodeRemoved', function () {
+        $('#thumbnail-container').css('margin-top', $(window).height() / 2 - $('#thumbnail-container').height() / 2);
+        $('.thumbnail').css('height', $('.thumbnail').width() / 16 * 9);
+    });
+    loadThumbs();
+    loadLinkbars();
 
-  $(window).on('resize', function() {
-    $('.thumbnail').css('height', $('.thumbnail').width()/16*9);
-    $('#thumbnail-container').css('margin-top', $(window).height()/2 - $('#thumbnail-container').height()/2);
-  });
+    $(window).on('resize', function () {
+        $('.thumbnail').css('height', $('.thumbnail').width() / 16 * 9);
+        $('#thumbnail-container').css('margin-top', $(window).height() / 2 - $('#thumbnail-container').height() / 2);
+    });
 
-  $('#thumbnail-container').css('margin-top', $(window).height()/2 - $('#thumbnail-container').height()/2);
+    $('#thumbnail-container').css('margin-top', $(window).height() / 2 - $('#thumbnail-container').height() / 2);
 
-  $('#settings').click(function() {loadSetup()});
+    $('#settings').click(function () { loadSetup() });
 });
--- js/setup.js
+++ js/setup.js
@@ -2,144 +2,144 @@
 const TEXTARE_FONT_SIZE = 'font-size: 8pt';
 
 function loadSetup() {
-  $('#thumbnail-container').empty();
-  $('#thumbnail-container').sortable({
-    items: '> .sortable'
-  });
-  $('.links-bar').empty();
+    $('#thumbnail-container').empty();
+    $('#thumbnail-container').sortable({
+        items: '> .sortable'
+    });
+    $('.links-bar').empty();
 
-  chrome.storage.sync.get(function(data) {
-    if (!data.curProfile) setProfile('default');
-    var thumbnails = data.profiles[data.curProfile];
+    browser.storage.sync.get(function (data) {
+        if (!data.curProfile) setProfile('default');
+        var thumbnails = data.profiles[data.curProfile];
 
-    for (var i = 0; i < thumbnails.thumbs.length; i++) {
-      $('#thumbnail-container').append(
-        $('<div />', {
-          class: 'thumbnail setup-thumb sortable'
-        })
-        .append($('<textarea />', {
-          rows: 3, cols: 35, placeholder: 'URL',
-          style: TEXTARE_FONT_SIZE,
-          text: thumbnails.thumbs[i].url
-        })).append('<br>')
-        .append($('<textarea />', {
-          rows: 4, cols: 35, placeholder: 'Image',
-          style: TEXTARE_FONT_SIZE,
-          text: thumbnails.thumbs[i].img
-        })).append('<br>')
-        .append($('<img />', {
-          class: 'remove-thumb clickable',
-          src: 'assets/icons/ic_clear_white_24px.svg',
-          click: function(e) {
-            $(this).parents("div:first").remove();
-          }
-        }))
-      )
-    }
-
-    $('#thumbnail-container').append(
-      $('<div />', {
-        class: 'thumbnail clickable settings-thumbnail add-thumb',
-        click: function(e) {
-          $(".add-thumb").before(
-            $('<div />', {
-              class: 'thumbnail setup-thumb sortable'
-            })
-            .append($('<textarea />', {
-              rows: 3, cols: 35, placeholder: 'URL',
-              style: TEXTARE_FONT_SIZE
-            }))
-            .append($('<textarea />', {
-              rows: 4, cols: 35, placeholder: 'Image',
-              style: TEXTARE_FONT_SIZE
-            }))
-            .append($('<img />', {
-              class: 'remove-thumb clickable',
-              src: 'assets/icons/ic_clear_white_24px.svg',
-              click: function(e) {
-                $(this).parents("div:first").remove();
-              }
-            }))
-          )
+        for (var i = 0; i < thumbnails.thumbs.length; i++) {
+            $('#thumbnail-container').append(
+                $('<div />', {
+                    class: 'thumbnail setup-thumb sortable'
+                })
+                    .append($('<textarea />', {
+                        rows: 3, cols: 35, placeholder: 'URL',
+                        style: TEXTARE_FONT_SIZE,
+                        text: thumbnails.thumbs[i].url
+                    })).append('<br>')
+                    .append($('<textarea />', {
+                        rows: 4, cols: 35, placeholder: 'Image',
+                        style: TEXTARE_FONT_SIZE,
+                        text: thumbnails.thumbs[i].img
+                    })).append('<br>')
+                    .append($('<img />', {
+                        class: 'remove-thumb clickable',
+                        src: 'assets/icons/ic_clear_white_24px.svg',
+                        click: function (e) {
+                            $(this).parents("div:first").remove();
+                        }
+                    }))
+            )
         }
-      })
-      .append($('<div />', {
-        class: 'thumbnail-background',
-        style: 'background-image: url(assets/icons/ic_add_circle_outline_white_24px.svg);'
-      }))
-    )
 
-    $('#thumbnail-container').append(
-      $('<div />', {
-        class: 'thumbnail clickable settings-thumbnail',
-        click: function(e) { saveSetup() }
-      })
-      .append($('<div />', {
-        class: 'thumbnail-background',
-        style: 'background-image: url(assets/icons/ic_save_white_24px.svg);'
-      }))
-    )
+        $('#thumbnail-container').append(
+            $('<div />', {
+                class: 'thumbnail clickable settings-thumbnail add-thumb',
+                click: function (e) {
+                    $(".add-thumb").before(
+                        $('<div />', {
+                            class: 'thumbnail setup-thumb sortable'
+                        })
+                            .append($('<textarea />', {
+                                rows: 3, cols: 35, placeholder: 'URL',
+                                style: TEXTARE_FONT_SIZE
+                            }))
+                            .append($('<textarea />', {
+                                rows: 4, cols: 35, placeholder: 'Image',
+                                style: TEXTARE_FONT_SIZE
+                            }))
+                            .append($('<img />', {
+                                class: 'remove-thumb clickable',
+                                src: 'assets/icons/ic_clear_white_24px.svg',
+                                click: function (e) {
+                                    $(this).parents("div:first").remove();
+                                }
+                            }))
+                    )
+                }
+            })
+                .append($('<div />', {
+                    class: 'thumbnail-background',
+                    style: 'background-image: url(assets/icons/ic_add_circle_outline_white_24px.svg);'
+                }))
+        )
 
-    $('#thumbnail-container').append(
-      $('<div />', {
-        class: 'thumbnail clickable settings-thumbnail',
-        click: function(e) { exportSettings() }
-      })
-      .append($('<div />', {
-        class: 'thumbnail-background',
-        style: 'background-image: url(assets/icons/ic_file_download_white_24px.svg);'
-      }))
-    )
+        $('#thumbnail-container').append(
+            $('<div />', {
+                class: 'thumbnail clickable settings-thumbnail',
+                click: function (e) { saveSetup() }
+            })
+                .append($('<div />', {
+                    class: 'thumbnail-background',
+                    style: 'background-image: url(assets/icons/ic_save_white_24px.svg);'
+                }))
+        )
 
-    $('#thumbnail-container').append(
-      $('<div />', {
-        class: 'thumbnail clickable settings-thumbnail',
-        click: function(e) { importSettings() }
-      })
-      .append($('<div />', {
-        class: 'thumbnail-background',
-        style: 'background-image: url(assets/icons/ic_file_upload_white_24px.svg);'
-      }))
-    )
+        $('#thumbnail-container').append(
+            $('<div />', {
+                class: 'thumbnail clickable settings-thumbnail',
+                click: function (e) { exportSettings() }
+            })
+                .append($('<div />', {
+                    class: 'thumbnail-background',
+                    style: 'background-image: url(assets/icons/ic_file_download_white_24px.svg);'
+                }))
+        )
 
-    $('#thumbnail-container').append($('<div />', { style: 'clear: left; display: block;' }))
+        $('#thumbnail-container').append(
+            $('<div />', {
+                class: 'thumbnail clickable settings-thumbnail',
+                click: function (e) { importSettings() }
+            })
+                .append($('<div />', {
+                    class: 'thumbnail-background',
+                    style: 'background-image: url(assets/icons/ic_file_upload_white_24px.svg);'
+                }))
+        )
 
-    for (linkbar in data.profiles[data.curProfile].linkbars) {
-      $('#links-bar-' + linkbar).append(
-        $('<textarea />', {
-          id: 'lb-' + linkbar + '-setup',
-          rows: 4, cols: 40,
-          text: JSON.stringify(data.profiles[data.curProfile].linkbars[linkbar], null, '  '),
-          style: 'max-height: 500px; max-width: 500px'
-        })
-      );
-    }
-  });
+        $('#thumbnail-container').append($('<div />', { style: 'clear: left; display: block;' }))
+
+        for (linkbar in data.profiles[data.curProfile].linkbars) {
+            $('#links-bar-' + linkbar).append(
+                $('<textarea />', {
+                    id: 'lb-' + linkbar + '-setup',
+                    rows: 4, cols: 40,
+                    text: JSON.stringify(data.profiles[data.curProfile].linkbars[linkbar], null, '  '),
+                    style: 'max-height: 500px; max-width: 500px'
+                })
+            );
+        }
+    });
 }
 
 function saveSetup() {
-  $('#thumbnail-container').sortable('destroy');
-  chrome.storage.sync.get(function(data) {
-    if (!data.curProfile || !data.profiles[data.curProfile].thumbs) return;
-    data.profiles[data.curProfile];
-    data.profiles[data.curProfile].thumbs = [];
-    for (var i = 0; i < $('.setup-thumb').length; i++) {
-      if ($('.setup-thumb').eq(i).find('textarea').eq(0).val().length > 0) {
-        data.profiles[data.curProfile].thumbs.push({
-          url: $('.setup-thumb').eq(i).find('textarea').eq(0).val(),
-          img: $('.setup-thumb').eq(i).find('textarea').eq(1).val()
+    $('#thumbnail-container').sortable('destroy');
+    browser.storage.sync.get(function (data) {
+        if (!data.curProfile || !data.profiles[data.curProfile].thumbs) return;
+        data.profiles[data.curProfile];
+        data.profiles[data.curProfile].thumbs = [];
+        for (var i = 0; i < $('.setup-thumb').length; i++) {
+            if ($('.setup-thumb').eq(i).find('textarea').eq(0).val().length > 0) {
+                data.profiles[data.curProfile].thumbs.push({
+                    url: $('.setup-thumb').eq(i).find('textarea').eq(0).val(),
+                    img: $('.setup-thumb').eq(i).find('textarea').eq(1).val()
+                });
+            }
+        }
+
+        for (linkbar in data.profiles[data.curProfile].linkbars) {
+            data.profiles[data.curProfile].linkbars[linkbar] = JSON.parse(($('#lb-' + linkbar + '-setup').val()).replace(/(?:\r\n|\r|\n)/g, ''));
+        }
+
+        browser.storage.sync.set(data, function () {
+            $('.links-bar').empty();
+            loadThumbs();
+            loadLinkbars();
         });
-      }
-    }
-
-    for (linkbar in data.profiles[data.curProfile].linkbars) {
-      data.profiles[data.curProfile].linkbars[linkbar] = JSON.parse(($('#lb-' + linkbar + '-setup').val()).replace(/(?:\r\n|\r|\n)/g, ''));
-    }
-
-    chrome.storage.sync.set(data, function() {
-      $('.links-bar').empty();
-      loadThumbs();
-      loadLinkbars();
     });
-  });
 }
